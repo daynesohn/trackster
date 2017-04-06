e834acfb94d6aab365430f5e861ff6f8480acacd
daynesohn <dayne.sohn@gmail.com>
2017-04-06 13:23:43 -0500

    Adding Spotify API

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,8 +1,14 @@
+$(document).ready(function() {
+  $('#search-button').click(function() {
+    Trackster.searchTracksByTitle($('#search-term').val());
+  })
+})
+
 var Trackster = {};
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
+  Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
 
@@ -13,5 +19,11 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+  $.ajax({
+    url: 'https://api.spotify.com/v1/search?type=track&q=' + title,
+    datatype: 'jsonp',
+    success: function(data) {
+      console.log(data);
+    }
+  })
 };
