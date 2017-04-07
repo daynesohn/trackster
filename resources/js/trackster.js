65197dcb5091295b2aeb80ca4c2cfb19849a047f
daynesohn <dayne.sohn@gmail.com>
2017-04-06 23:13:04 -0500

    Implementing Spotify API

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -11,7 +11,26 @@ var Trackster = {};
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
+  $('#tracklist').empty();
 
+  for (var trackIndex = 0; trackIndex < tracks.length; trackIndex++) {
+    var track = tracks[trackIndex];
+    var $trackList = $('#tracklist')
+    var htmlTrackRow =
+    '<div class="row track-data">' +
+    '<div class="col-xs-1 col-xs-offset-1"><a href="' + track.preview_url + '" target="_blank"><i class="fa fa-play-circle-o fa-2x"></i></a></div>' +
+    '<div class="col-xs-4 title">' + track.name +
+    '</div>' +
+    '<div class="col-xs-2 title">' + track.artists[0].name +
+    '</div>' +
+    '<div class="col-xs-2 title">' + track.album.name +
+    '</div>' +
+    '<div class="col-xs-2 title">' + track.popularity + '</div>' +
+    '</div>';
+
+    $trackList.append(htmlTrackRow);
+    console.log(tracks[trackIndex]);
+  }
 };
 
 /*
@@ -22,8 +41,8 @@ Trackster.searchTracksByTitle = function(title) {
   $.ajax({
     url: 'https://api.spotify.com/v1/search?type=track&q=' + title,
     datatype: 'jsonp',
-    success: function(data) {
-      console.log(data);
+    success: function(response) {
+      Trackster.renderTracks(response.tracks.items);
     }
   })
 };
